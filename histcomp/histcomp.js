3f160f2be755d2fe963eca6502cbf3d266ae988e
luislrl <luiseduardolrlima@gmail.com>
2025-06-25 21:12:15 -0300

    Initial app

---
+++ histcomp/histcomp.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    var message = document.getElementById("message");
+    if (message.style.display === "none") {
+        message.style.display = "block";
+    } else {
+        message.style.display = "none";
+    }
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("div1").onclick = function() {
+    window.location.href = "histcomp/histcomp.html";
+};
+document.getElementById("div2").onclick = function() {
+    window.location.href = "carreiras/carreiras.html";
+};
